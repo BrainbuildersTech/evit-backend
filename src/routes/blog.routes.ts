d4b4a94a6bfe7b4ff832d061493338b92b6814c1
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2026-06-29 12:49:19 +0100

    Pull here

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -5,14 +5,17 @@ import {
   deleteBlog,
   getBlogById,
   getBlogs,
+  getFilteredBlogs,
   updateBlog,
   upload
 } from '../controllers/blog.controller';
+import { adminInterceptor } from '../middleware/admin.middleware';
 
 const router = Router();
 
-router.post('/', upload.single("file"), createBlog);
-router.get('/', getBlogs);
-router.route('/:id').get(getBlogById).patch(upload.single("file"), updateBlog).delete(deleteBlog);
+router.post('/', adminInterceptor, upload.single("file"), createBlog);
+router.get('/', adminInterceptor, getBlogs);
+router.get('/published', getFilteredBlogs);
+router.route('/:id').get(getBlogById).patch(adminInterceptor, upload.single("file"), updateBlog).delete(adminInterceptor, deleteBlog);
 
 export default router;
